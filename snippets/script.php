18b757bc785bcc2fba1ce1f18e59995a3b239cd3
Maurice Kühlborn <mk@minimalweb.de>
2020-04-23 15:58:58 +0200

    Set anonymize-ip after create

--- snippets/script.php
+++ snippets/script.php
@@ -17,12 +17,11 @@
   (p[g].q=p[g].q||[]).push(arguments)},p[g].l=1*new Date();e=b.createElement(o),
   s=b.getElementsByTagName(o)[0];e.async=1;e.src=r;s.parentNode.insertBefore(e,s)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
+  
+  ga('create', '<?= c::get("google.analytics", "") ?>', 'auto');
   <?php if (c::get('google.analytics.anonymize-ip', null)) : ?>
     ga('set', 'anonymizeIp', true);
   <?php endif ?>
-  
-  ga('create', '<?= c::get("google.analytics", "") ?>', 'auto');
   ga('send', 'pageview');
 </script>
 <?php endif ?>
