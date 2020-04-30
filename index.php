819433cefea7d3e6d50c9ed9a9f1d21d0b80160e
Maurice Kühlborn <mk@minimalweb.de>
2020-04-30 11:23:57 +0200

    Add default english and german translation to plugin

--- index.php
+++ index.php
@@ -3,5 +3,13 @@
 Kirby::plugin('minimalweb/google-analytics', [
     'snippets' => [
         'google-analytics' => __DIR__ . '/snippets/script.php'
+    ],
+    'translations' => [
+        'en' => [
+            'google.analytics.success-alert' => 'Google Analytics tracking for this website and browser has been successfully disabled.',
+        ],
+        'de' => [
+            'google.analytics.success-alert' => 'Das Google-Analytics-Tracking für diese Website wurde in diesem Browser erfolgreich deaktiviert.',
+        ]
     ]
 ]);
